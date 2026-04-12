ec19c6142d41db8e213d28dca13b92e471dce3af
Mohamed Amine <nafiai.medamine@gmail.com>
2026-04-12 13:04:35 +0100

    modif le model connectorstatus

--- app/Models/ConnectorStatus.php
+++ app/Models/ConnectorStatus.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class ConnectorStatus extends Model
 {
-    //
+    protected $fillable = [
+        'connector_id',
+        'status',
+        'updated_by',
+        'last_updated_at',
+    ];
+
+    public $timestamps = false; // car on utilise last_updated_at
+
+    public function connector()
+    {
+        return $this->belongsTo(Connector::class);
+    }
 }
