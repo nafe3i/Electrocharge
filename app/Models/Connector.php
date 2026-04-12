30c6be8a3813442f53f825aef890c56133a9af1e
Mohamed Amine <nafiai.medamine@gmail.com>
2026-04-12 13:04:03 +0100

    creer le model connector

--- app/Models/Connector.php
+++ app/Models/Connector.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Connector extends Model
 {
-    protected $fillable=['station_id','type','power_kw','price_per_kwl','quantity'];
+    protected $fillable = [
+        'station_id',
+        'type',
+        'power_kw',
+        'price_per_kwh', 
+        'quantity',
+    ];
+
+    public function station()
+    {
+        return $this->belongsTo(Station::class);
+    }
+
+    public function status()
+    {
+        return $this->hasOne(ConnectorStatus::class);
+    }
 }
