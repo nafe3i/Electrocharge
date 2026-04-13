1ea1bc932a889ced0c5d38a329201cef86126c1f
Mohamed Amine <nafiai.medamine@gmail.com>
2026-04-13 09:28:07 +0100

    creation de model hisoire de rehcrche avec les relation

--- app/Models/SearchHistory.php
+++ app/Models/SearchHistory.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SearchHistory extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'query',
+        'latitude',
+        'longitude',
+        'filters',
+    ];
+
+    protected $casts = [
+        'filters' => 'array',
+        'latitude' => 'float',
+        'longitude' => 'float',
+        'created_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
