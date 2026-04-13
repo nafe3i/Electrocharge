0b196de4ffd84439adf3149b3c963e910a403c74
Mohamed Amine <nafiai.medamine@gmail.com>
2026-04-13 09:27:38 +0100

    creation de model review avec les relation

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'station_id',
+        'rating',
+        'comment',
+    ];
+
+    protected $casts = [
+        'rating' => 'integer',
+        'created_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function station(): BelongsTo
+    {
+        return $this->belongsTo(Station::class);
+    }
 }
