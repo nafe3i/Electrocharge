8e608dcabb61fc1fb3adadc9e569c2a6dad71184
Mohamed Amine <nafiai.medamine@gmail.com>
2026-04-13 09:25:51 +0100

    creation de model favories avec les relation

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Favorite extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'station_id',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function station(): BelongsTo
+    {
+        return $this->belongsTo(Station::class);
+    }
 }
