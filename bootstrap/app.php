5f6421b4bbbea6d0c911651e1ef6aa8c75115b99
Mohamed Amine <nafiai.medamine@gmail.com>
2026-04-21 12:30:10 +0100

    chore: update bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,13 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
+use Illuminate\Console\Scheduling\Schedule;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-    )
+    )->withSchedule(function (Schedule $schedule) {
+        $schedule->command('stations:sync')->dailyAt('00:00');
+        $schedule->command('connector:simulate')->everyFifteenMinutes();
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => RoleMiddleware::class,
